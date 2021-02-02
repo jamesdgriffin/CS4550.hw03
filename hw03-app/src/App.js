aec703f1dc9f5d81a067335466d0a20d9f2008b1
yono-oko <57371137+yono-oko@users.noreply.github.com>
2021-02-02 16:04:03 -0500

    gettin started

---
+++ hw03-app/src/App.js
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [secret, setSecret] = useState("1234");
+  const [guesses, setGuesses] = useState([]);
+  const [text, setText] = useState("");
+
+  function guess(ev) {
+    let input = ev.target.value;
+    setGuesses(guesses.concat(input));
+    setText(input);
+  }
+
+
+
+  return (
+    <div className="App">
+      <h1>secret: {secret}</h1>
+      <input type="text" value={text} onChange={guess} />
+      <h1>guesses: {guesses.join('')}</h1>
+      <p>
+        <button onClick={() => setGuesses([])}>Reset</button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
