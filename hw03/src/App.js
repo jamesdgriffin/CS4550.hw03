345451513d0c3525ef9b86170bc7df1198c370a8
yono-oko <griffin.jame@northeastern.edu>
2021-02-03 17:44:50 -0500

    workin

--- hw03/src/App.js
+++ hw03/src/App.js
@@ -1,12 +1,16 @@
 import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './game.js';
+import {viewGuesses} from './game.js';
 
 function App() {
   const [secret, setSecret] = useState("1234");
   const [guesses, setGuesses] = useState([]);
   const [text, setText] = useState("");
+
+  function viewLoGuesses() {
+    console.log(viewGuesses(guesses));
+  }
 
   function changeText(ev) {
     setText(ev.target.value);
@@ -22,14 +26,22 @@ function App() {
     setText("");
   }
 
+  function enter(ev) {
+    if (ev.key == "Enter") {
+      guess();
+    }
+  }
+
   return (
     <div className="App">
       <h1>secret: {secret}</h1>
-      <input type="text" value={text} onChange={changeText} />
+      <input type="text" value={text} onChange={changeText}
+                onKeyPress={enter}/>
       <h1>guesses: {guesses.join('')}</h1>
       <p>
         <button onClick={guess}>Guess</button>
         <button onClick={reset}>Reset</button>
+
       </p>
     </div>
   );
---
+++ hw03/src/game.js
@@ -0,0 +1,12 @@
+export function viewGuesses(guesses) {
+
+  let output = [];
+
+  output.push(guesses);
+
+  return(
+      <p>Previous Guesses: {output}</p>
+  );
+
+
+}
